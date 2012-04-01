5a39a2249d550d259a16239b542a71c0805673d4
Ryan Zander <exide@hotmail.com>
2012-04-01 09:15:50 +0700

    refactored the status view to allow easier grokking at a glance

--- views.py
+++ views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from models import Server
 
-def status(request, server_id):
-    return render_to_response(
-        'ventrilo/status.html',
-        {'server': Server.objects.get(pk=server_id) },
-        context_instance=RequestContext(request))
+def status(request, server_id, template_name='ventrilo/status.html'):
+
+    kwargs = dict()
+    kwargs['server'] = Server.objects.get(pk=server_id)
+
+    context = RequestContext(request)
+
+    return render_to_response(template_name, kwargs, context_instance=context)
